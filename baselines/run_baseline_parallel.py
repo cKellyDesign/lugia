ac2dbf4ba0be055f30cb206283e31d03e2a12134
Mathieu D <mathieu.dreano@decathlon.com>
2024-03-03 15:30:23 +0100

    logging for tensorboard
    Signed-off-by: Mathieu D <mathieu.dreano@decathlon.com>

--- baselines/run_baseline_parallel.py
+++ baselines/run_baseline_parallel.py
@@ -1,12 +1,15 @@
 from os.path import exists
 from pathlib import Path
 import uuid
+
+from tensorboard_callback import TensorboardCallback
 from red_gym_env import RedGymEnv
 from stable_baselines3 import A2C, PPO
 from stable_baselines3.common import env_checker
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
 from stable_baselines3.common.utils import set_random_seed
-from stable_baselines3.common.callbacks import CheckpointCallback
+from stable_baselines3.common.callbacks import CheckpointCallback, CallbackList
+
 
 def make_env(rank, env_conf, seed=0):
     """
@@ -38,14 +41,14 @@ if __name__ == '__main__':
             }
     
     
-    num_cpu = 44 #64 #46  # Also sets the number of episodes per training iteration
+    num_cpu = 4 #64 #46  # Also sets the number of episodes per training iteration
     env = SubprocVecEnv([make_env(i, env_config) for i in range(num_cpu)])
     
     checkpoint_callback = CheckpointCallback(save_freq=ep_length, save_path=sess_path,
                                      name_prefix='poke')
     #env_checker.check_env(env)
     learn_steps = 40
-    file_name = 'session_e41c9eff/poke_38207488_steps' #'session_e41c9eff/poke_250871808_steps'
+    file_name = '' #'session_e41c9eff/poke_250871808_steps'
     
     #'session_bfdca25a/poke_42532864_steps' #'session_d3033abb/poke_47579136_steps' #'session_a17cc1f5/poke_33546240_steps' #'session_e4bdca71/poke_8945664_steps' #'session_eb21989e/poke_40255488_steps' #'session_80f70ab4/poke_58982400_steps'
     if exists(file_name + '.zip'):
@@ -57,7 +60,8 @@ if __name__ == '__main__':
         model.rollout_buffer.n_envs = num_cpu
         model.rollout_buffer.reset()
     else:
-        model = PPO('CnnPolicy', env, verbose=1, n_steps=ep_length, batch_size=512, n_epochs=1, gamma=0.999)
-    
+        model = PPO('CnnPolicy', env, verbose=1, n_steps=ep_length, batch_size=512, n_epochs=1, gamma=0.999, tensorboard_log='')
+
+    callbacks = [checkpoint_callback, TensorboardCallback()]
     for i in range(learn_steps):
-        model.learn(total_timesteps=(ep_length)*num_cpu*1000, callback=checkpoint_callback)
+        model.learn(total_timesteps=(ep_length)*num_cpu*1000, callback=CallbackList(callbacks))
